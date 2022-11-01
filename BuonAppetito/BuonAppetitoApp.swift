248ec00494a3362953e94b188afbee7f61e2f626
ribotinho <ribotinho@gmail.com>
2022-11-01 18:01:49 +0100

    Initial Commit

---
+++ BuonAppetito/BuonAppetitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuonAppetitoApp.swift
+//  BuonAppetito
+//
+//  Created by Pau Ribot Pujolras on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BuonAppetitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
